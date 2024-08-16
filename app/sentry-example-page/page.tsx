87ab9fc5d4fd979a66cc896a0d1e6ca04622df40
DRAGONSTONE\Chathuranga <csampath.personal@gmail.com>
2024-08-16 23:49:42 +0530

    fix: Build issues

--- app/sentry-example-page/page.tsx
+++ app/sentry-example-page/page.tsx
@@ -36,32 +36,7 @@ export default function Page() {
         </h1>
 
         <p>Get started by sending us a sample error:</p>
-        <button
-          type="button"
-          style={{
-            padding: "12px",
-            cursor: "pointer",
-            backgroundColor: "#AD6CAA",
-            borderRadius: "4px",
-            border: "none",
-            color: "white",
-            fontSize: "14px",
-            margin: "18px",
-          }}
-          onClick={async () => {
-            await Sentry.startSpan({
-              name: 'Example Frontend Span',
-              op: 'test'
-            }, async () => {
-              const res = await fetch("/api/sentry-example-api");
-              if (!res.ok) {
-                throw new Error("Sentry Example Frontend Error");
-              }
-            });
-          }}
-        >
-          Throw error!
-        </button>
+
 
         <p>
           Next, look for the error on the{" "}
