4b4e5e80e660b2a21b41bc12f1434baf664b7ffe
DRAGONSTONE\Chathuranga <csampath.personal@gmail.com>
2025-01-05 23:55:06 +0530

    feat: Add AWS service list bottom banner
    Additional Changes:
    - Resize AWS badge at top

--- components/ui/ResumeScreen.tsx
+++ components/ui/ResumeScreen.tsx
@@ -9,7 +9,7 @@ const ResumeScreen = () => {
   const cvContainerZoomed = {
     top: "0.00000001%",
     left: "0.00000001%",
-    zIndex: "5",
+    zIndex: "20",
     opacity: 1,
     height: "100%",
     width: "100%",
@@ -65,24 +65,28 @@ const ResumeScreen = () => {
       <div
         onClick={() => onCVClick()}
         className={`absolute duration-500 bg-transparent cursor-pointer md:w-[50rem] w-[25rem] md:h-[52rem] h-[35rem] `} style={returnContainerStyles()}>
-        <Image 
+        <Image
           layout="responsive"
           width={792}
           height={2048}
-          src="https://d1k2af8sw8x9is.cloudfront.net/resume-pdf-img/output.png" 
-          alt="Fetched from S3" 
-          className={`absolute ${toggleZoom === false && toggleMouseInOut === false && 'animate-faderMd'} `} />
-          <div className='absolute over right-0 top-0 w-fit opacity-80 bg-amber-800 flex flex-row items-center shadow-md
+          src="https://d1k2af8sw8x9is.cloudfront.net/resume-pdf-img/output.png"
+          alt="Fetched from S3"
+          className={`${toggleZoom === false && toggleMouseInOut === false && 'animate-faderMd'} `} />
+        <div className='absolute over right-0 top-0 w-fit opacity-80 bg-amber-800 flex flex-row items-center shadow-md
           pt-[0.0rem] pl-[1rem] pr-[.0rem] pb-[.0rem]
           rounded-tl-[0px] rounded-tr-[0px] rounded-br-[0px] rounded-bl-[10px]
-          '>Generated via
-          <Image 
+          '>Auto-Updated via
+          <Image
             width={50}
             height={50}
-            className='w-14 h-12 ml-2 bg-slate-200 rounded-sm pr-1 pl-1 duration-100 hover:scale-[1.02] hover:-translate-x-[.05rem] hover:translate-y-[.05rem]' 
+            className='w-12 h-10 ml-2 bg-slate-200 rounded-sm pr-1 pl-1 duration-100 hover:scale-[1.02] hover:-translate-x-[.05rem] hover:translate-y-[.05rem]'
             alt='aws-logo'
             src='/aws2.png'></Image>
-          </div>
+        </div>
+        <div className='relative bottom-0 flex flex-col items-center bg-slate-700 p-2 -mt-6'>
+          <span className='text-stone-300'>This Image is automatically updated using:</span>
+          <span className='justify-center text-amber-100'>GitHub Webhooks + AWS API Gateway + S3 + AWS Lambda + EventBridge + CloudFront</span>
+        </div>
       </div>
       <div
         onMouseEnter={() => onCVMouseEnter(true)}
